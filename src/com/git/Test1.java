c76993d76ead98f71a02ae0adab40f14d763754e
shubham <shubhamgogawale1@gmail.com>
2022-12-22 15:19:55 +0530

    First commit....

---
+++ src/com/git/Test1.java
@@ -0,0 +1,13 @@
+package com.git;
+
+public class Test1 {
+	public static void main(String[] args) {
+		String hungry = "yes";
+		if (hungry.equals("yes")) {
+			System.out.println("Eat Somthing");
+		} else {
+			System.out.println("Don't Eat");
+		}
+	}
+
+}
